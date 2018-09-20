e975f71fcad6b01bd28696e9d0f30bab818593f5
Karthik Venkateswaran <karthikvt26@users.noreply.github.com>
2018-09-20 19:54:49 +0530

    Issue 18 (#19)
    * fix #18 and unstages package-lock.json

--- src/lib/components/AuthConsumer.js
+++ src/lib/components/AuthConsumer.js
@@ -1,20 +1,3 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
 import { Consumer } from '../context';
 
-class AuthConsumer extends React.Component {
-  render() {
-    return (
-      <Consumer>
-        { (props) => this.props.children( { ...props })}
-      </Consumer>
-    );
-  }
-};
-
-AuthConsumer.propTypes = {
-  children: PropTypes.func.isRequired,
-};
-
-export default AuthConsumer;
+export default Consumer;
